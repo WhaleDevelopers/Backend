7b212e3ee45bdba642da1b34e10a6a18f90f5055
hosik99 <wnddl777@gmail.com>
2025-10-07 15:28:33 +0900

    Initial commit

---
+++ src/test/java/com/kim/devstu/DevStuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.devstu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
